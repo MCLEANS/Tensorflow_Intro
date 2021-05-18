a58dc09084ea0ff99900bb46e403ad309d767a98
MCLEANS <jackagwaya@gmail.com>
2021-05-18 21:51:06 +0300

    created first session

--- Lecture_1/intro.py
+++ Lecture_1/intro.py
@@ -1,7 +1,11 @@
 import tensorflow as tf
 
-a = tf.add(3,5,)
-b = tf.add(3.0,4.6)
+project_graph = tf.Graph()
+with project_graph.as_default():
+    a = tf.add(3,5,)
+    b = tf.add(3.0,4.6)
 
-print(a)
-print(b)
+with tf.compat.v1.Session(graph = project_graph) as sess:
+    a,b = sess.run([a,b])
+    print(a)
+    print(b)
