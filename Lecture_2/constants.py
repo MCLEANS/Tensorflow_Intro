5fb6809103098bf66e0c490d2b80c3352dae4199
MCLEANS <jackagwaya@gmail.com>
2021-05-23 22:53:05 +0300

    Modifed constants

--- Lecture_2/constants.py
+++ Lecture_2/constants.py
@@ -3,9 +3,15 @@ import tensorflow as tf
 graph = tf.Graph()
 
 with graph.as_default():
-    a = tf.add(3,5)
-
-
+    a = tf.constant([3,4], name = "input_data")
+    b = tf.constant([[2,3],[4,6]], name = "variables")
+    x = tf.add(a,b, name = 'Addition')
+    y = tf.multiply(a,b, name = "multiplication")
 
 with tf.compat.v1.Session(graph = graph) as sess:
-    print(sess.run(a))
+    writer = tf.compat.v1.summary.FileWriter("./graphs",sess.graph)
+    x,y = sess.run([x,y])
+    print(x)
+    print(y)
+
+writer.close()
